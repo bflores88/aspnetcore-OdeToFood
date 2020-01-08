0f1acc00809cbf9deef419867b3a5460a76986c4
Brenda Flores <kmietotwy@gmail.com>
2020-01-08 13:24:31 -0800

    created delete page

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = _restaurantData.Delete(restaurantId);
+            _restaurantData.Commit();
+
+            if (restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            TempData["Message"] = $"{restaurant.Name} deleted";
+            return RedirectToPage("./List");
+        }
+    }
+}
