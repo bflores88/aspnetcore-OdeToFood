79ed1e457ffaceb67e1829df56865ae3344a305a
Brenda Flores <kmietotwy@gmail.com>
2020-01-08 13:47:23 -0800

    updated missing OdeToFood.Core and replaced List table with partial

--- OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using OdeToFood.core;
+using OdeToFood.Core;
 
 namespace OdeToFood.Data
 {
--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using OdeToFood.core;
+using OdeToFood.Core;
 using OdeToFood.Data;
 
 namespace OdeToFood
